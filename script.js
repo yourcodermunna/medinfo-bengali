e690f3ba4ab472d4f3a60f1cf421a82694954ec5
yourcodermunna <munnabanik.pharma@gmail.com>
2025-04-05 17:49:21 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 function openChatbot() {
-    alert("চ্যাটবট শীঘ্রই উপলব্ধ হবে!");
-}
-
-function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
-        if (section.style.display === "none" || section.style.display === "") {
-            section.style.display = "block";
-        } else {
-            section.style.display = "none";
-        }
-    }
-}
-
+    alert("চ্যাটবট খুলছে...");
 }
